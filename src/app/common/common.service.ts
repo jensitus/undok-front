df177e4f7273202c796a10d9cad5481c24b49e57
Jens <jens@ist-ur.org>
2018-12-07 09:33:49 +0100

    create Todo

--- src/app/common/common.service.ts
+++ src/app/common/common.service.ts
@@ -9,12 +9,21 @@ export class CommonService {
   constructor() { }
 
   public demoSubject: BehaviorSubject<any> = new BehaviorSubject(null);
+  public todoSubject: BehaviorSubject<boolean> = new BehaviorSubject(null);
 
   setSubject(value) {
     if (value) {
       this.demoSubject.next(value);
     } else {
       this.demoSubject.next(null);
+    }
+  }
+
+  setNewTodoSubject(value) {
+    if (value) {
+      this.todoSubject.next(value);
+    } else {
+      this.todoSubject.next(null);
     }
   }
 
--- src/app/todo-item/list-todo/list-todo.component.ts
+++ src/app/todo-item/list-todo/list-todo.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {AlertService} from '../../auth/services/alert.service';
 import {TodoService} from '../services/todo.service';
+import {CommonService} from '../../common/common.service';
 
 @Component({
   selector: 'app-list-todo',
@@ -12,15 +13,29 @@ export class ListTodoComponent implements OnInit {
 
   todos: any;
   error: any;
+  reload = false;
 
   constructor(
     private router: Router,
     private todoService: TodoService,
-    private alertService: AlertService
+    private alertService: AlertService,
+    private commonService: CommonService
   ) {
   }
 
   ngOnInit() {
+    this.getTodos();
+    this.commonService.todoSubject.subscribe(res => {
+      this.reload = res;
+      if (this.reload) {
+        console.log('THE NEW DONNER OR HIMMEL TODO');
+        console.log(this.reload);
+        this.getTodos();
+      }
+    });
+  }
+
+  private getTodos() {
     this.todoService.getTodos().subscribe(
       (data) => {
         this.todos = data;
