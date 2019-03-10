a0b6a56db9f3656a53749821e995f23ae4451aad
Jens <jens@ist-ur.org>
2019-03-10 22:53:49 +0100

    delete todos

--- src/app/todo-item/list-todo/list-todo.component.ts
+++ src/app/todo-item/list-todo/list-todo.component.ts
@@ -3,6 +3,7 @@ import {Router} from '@angular/router';
 import {AlertService} from '../../common/alert/services/alert.service';
 import {TodoService} from '../services/todo.service';
 import {CommonService} from '../../common/common.service';
+import {Todo} from '../model/todo';
 
 @Component({
   selector: 'app-list-todo',
@@ -14,6 +15,8 @@ export class ListTodoComponent implements OnInit {
   todos: any;
   error: any;
   reload = false;
+  loading = false;
+  todo: any;
 
   constructor(
     private router: Router,
@@ -25,13 +28,7 @@ export class ListTodoComponent implements OnInit {
 
   ngOnInit() {
     this.getTodos();
-    this.commonService.todoSubject.subscribe(res => {
-      this.reload = res;
-      if (this.reload) {
-        console.log(this.reload);
-        this.getTodos();
-      }
-    });
+    this.getReloadFromCommenService();
   }
 
   private getTodos() {
@@ -49,6 +46,49 @@ export class ListTodoComponent implements OnInit {
         }
       }
     );
+  }
+
+  updateTodo(todo_id) {
+    this.loading = true;
+    this.todoService.getTodo(todo_id).subscribe(data => {
+      this.todo = data;
+      this.todo.done = !this.todo.done;
+      console.log(this.todo.done);
+      console.log(!this.todo.done);
+      this.todoService.updateTodo(todo_id, this.todo).subscribe(updateSuccess => {
+        if (this.todo.done === true) {
+          this.alertService.success('Todo is really done');
+        } else if (this.todo.done === false) {
+          this.alertService.success('this todo is not done yet');
+        }
+        this.loading = false;
+        this.getTodos();
+      });
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  deleteTodo(todo_id) {
+    this.loading = true;
+    this.todoService.deleteTodo(todo_id).subscribe(data => {
+      console.log(data);
+      this.getTodos();
+      this.loading = false;
+    }, error => {
+      console.log(error);
+    });
+
+  }
+
+  private getReloadFromCommenService() {
+    this.commonService.todoSubject.subscribe(res => {
+      this.reload = res;
+      if (this.reload) {
+        console.log(this.reload);
+        this.getTodos();
+      }
+    });
   }
 
 }
