a660e640ec12372e0c17c619a2e838411a51eba3
jensitus <jens@service-b.org>
2021-08-01 20:10:23 +0200

    undok instead of urorg

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/urorg'));
+app.use(express.static('./dist/undok'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/urorg/'}),
+  res.sendFile('index.html', {root: 'dist/undok/'}),
 );
 
 // Start the app by listening on the default Heroku port
