da905e2c3aa2b0745e8654b305239c59a1c64528
jensitus <jens@service-b.org>
2022-04-22 11:38:38 +0200

    UNDOK-10 confirm modal

--- src/app/client/show-client-employers/show-client-employers.component.ts
+++ src/app/client/show-client-employers/show-client-employers.component.ts
@@ -8,75 +8,94 @@ import {ClientEmployerJobDescription} from '../model/client-employer-job-descrip
 import {EditClientEmployerJobDescriptionComponent} from '../edit-client-employer-job-description/edit-client-employer-job-description.component';
 
 @Component({
-  selector: 'app-show-client-employers',
-  templateUrl: './show-client-employers.component.html',
-  styleUrls: ['./show-client-employers.component.css']
+    selector: 'app-show-client-employers',
+    templateUrl: './show-client-employers.component.html',
+    styleUrls: ['./show-client-employers.component.css']
 })
 export class ShowClientEmployersComponent implements OnInit, OnDestroy {
 
-  @Input() clientId: string;
-  clientEmployerJobDescriptions: ClientEmployerJobDescription[];
-  private unsubscribe$ = new Subject();
-  private closeResult: string;
-  public isCollapsed = true;
+    @Input() clientId: string;
+    clientEmployerJobDescriptions: ClientEmployerJobDescription[];
+    private unsubscribe$ = new Subject();
+    public closeResult: string;
+    public isCollapsed = true;
 
-  constructor(
-    private employerService: EmployerService,
-    private commonService: CommonService,
-    private modalService: NgbModal
-  ) { }
-
-  private static getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
+    constructor(
+        private employerService: EmployerService,
+        private commonService: CommonService,
+        private modalService: NgbModal
+    ) {
     }
-  }
 
-  ngOnInit(): void {
-    this.getEmployersForClient();
-    this.getAddEmployerSubject();
-  }
+    private static getDismissReason(reason: any): string {
+        if (reason === ModalDismissReasons.ESC) {
+            return 'by pressing ESC';
+        } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+            return 'by clicking on a backdrop';
+        } else {
+            return `with: ${reason}`;
+        }
+    }
 
-  ngOnDestroy(): void {
-    this.unsubscribe$.next(true);
-  }
-
-  getEmployersForClient() {
-    this.employerService.getEmployersForClient(this.clientId).pipe(takeUntil(this.unsubscribe$)).subscribe(res => {
-      console.log('employers', res);
-      this.clientEmployerJobDescriptions = res;
-    });
-  }
-
-  deleteEmployerFromClient(e_id: string) {
-    this.employerService.deleteEmployerFromClient(e_id, this.clientId).pipe(takeUntil(this.unsubscribe$)).subscribe(r => {
-      this.commonService.setEmployerSubject(true);
-    });
-  }
-
-  getAddEmployerSubject() {
-    this.commonService.employerSubject.pipe(takeUntil(this.unsubscribe$)).subscribe(reload => {
-      if (reload === true) {
-        console.log('employer Subject');
+    ngOnInit(): void {
         this.getEmployersForClient();
+        this.getAddEmployerSubject();
+    }
+
+    ngOnDestroy(): void {
+        this.unsubscribe$.next(true);
+    }
+
+    getEmployersForClient() {
+        this.employerService.getEmployersForClient(this.clientId).pipe(takeUntil(this.unsubscribe$)).subscribe(res => {
+            console.log('employers', res);
+            this.clientEmployerJobDescriptions = res;
+        });
+    }
+
+    deleteEmployerFromClient(e_id: string) {
+        this.employerService.deleteEmployerFromClient(e_id, this.clientId).pipe(takeUntil(this.unsubscribe$)).subscribe(r => {
+            this.commonService.setEmployerSubject(true);
+        });
+    }
+
+    getAddEmployerSubject() {
+        this.commonService.employerSubject.pipe(takeUntil(this.unsubscribe$)).subscribe(reload => {
+            if (reload === true) {
+                console.log('employer Subject');
+                this.getEmployersForClient();
+                this.modalService.dismissAll();
+            }
+        });
+    }
+
+    edit(clientEmployerJobDescription: ClientEmployerJobDescription) {
+        const assignModal = this.modalService.open(EditClientEmployerJobDescriptionComponent, {
+            ariaLabelledBy: 'modal-basic-title',
+            size: 'lg'
+        });
+        assignModal.componentInstance.clientEmployerJobDescription = clientEmployerJobDescription;
+        assignModal.componentInstance.clientId = this.clientId;
+        assignModal.result.then((result) => {
+            this.closeResult = `Closed with: ${result}`;
+        }, (reason) => {
+            this.closeResult = `Dismissed ${ShowClientEmployersComponent.getDismissReason(reason)}`;
+        });
+    }
+
+    openDeleteConfirmationModal(content) {
+        this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+            this.closeResult = `Closed with: ${result}`;
+        }, (reason) => {
+            this.closeResult = `Dismissed ${ShowClientEmployersComponent.getDismissReason(reason)}`;
+        });
+    }
+
+    yes(employerId) {
+        this.deleteEmployerFromClient(employerId);
+    }
+
+    no() {
         this.modalService.dismissAll();
-      }
-    });
-  }
-
-  edit(clientEmployerJobDescription: ClientEmployerJobDescription) {
-    const assignModal = this.modalService.open(EditClientEmployerJobDescriptionComponent, {ariaLabelledBy: 'modal-basic-title', size: 'lg'});
-    assignModal.componentInstance.clientEmployerJobDescription = clientEmployerJobDescription;
-    assignModal.componentInstance.clientId = this.clientId;
-    assignModal.result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${ShowClientEmployersComponent.getDismissReason(reason)}`;
-    });
-  }
-
+    }
 }
